74733816b2ae08fc7ff7f9328dbb28969c14aaf6
Marcelo Forets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2017-02-17 09:30:31 +0100

    updated src note

--- numMonTot.m
+++ numMonTot.m
@@ -1,3 +1,4 @@
+%================================================================================================
 % Returns the TOTAL number of monomials in n variables of total degree UP TO d
 
 % INPUTS:
@@ -7,10 +8,7 @@
 % OUTPUTS:
 % monTot - TOTAL number of monomials in n variables of order UP TO d
 
-% NOTE: this helper function is from the Gloptipoly library, see ROA Matlab codes in
-% http://homepages.laas.fr/henrion/
-% and references therein.
-
+%================================================================================================
 function monTot = numMonTot(n,d)
     monTot = nchoosek(n+d,n);
 end
